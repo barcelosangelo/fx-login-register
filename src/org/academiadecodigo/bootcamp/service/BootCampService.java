adb80304c7b31074c5792c704d2021a7c37e5e7f
Angelo Barcelos <barcelos.angelo@gmail.com>
2018-01-04 10:28:51 -0100

    changes to MockBootCampService

--- src/org/academiadecodigo/bootcamp/service/BootCampService.java
+++ src/org/academiadecodigo/bootcamp/service/BootCampService.java
@@ -1,12 +1,17 @@
 package org.academiadecodigo.bootcamp.service;
 
 import org.academiadecodigo.bootcamp.model.Bootcamp;
+import org.academiadecodigo.bootcamp.model.CodeCadet;
 
 public interface BootCampService {
 
     Bootcamp findById(int id);
 
     void addBootcamp(Bootcamp bootcamp);
+
+    void addCodeCadet(CodeCadet codeCadet);
+
+    void finCodeCadet(CodeCadet codeCadet);
 
 
 }
--- src/org/academiadecodigo/bootcamp/service/MockBootCampService.java
+++ src/org/academiadecodigo/bootcamp/service/MockBootCampService.java
@@ -1,4 +1,32 @@
 package org.academiadecodigo.bootcamp.service;
 
-public class MockBootCampService {
+import org.academiadecodigo.bootcamp.model.Bootcamp;
+import org.academiadecodigo.bootcamp.model.CodeCadet;
+
+import java.util.Map;
+
+public class MockBootCampService implements BootCampService{
+
+    private Map<Integer, Bootcamp>  id;
+
+
+    @Override
+    public Bootcamp findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void addBootcamp(Bootcamp bootcamp) {
+
+    }
+
+    @Override
+    public void addCodeCadet(CodeCadet codeCadet) {
+
+    }
+
+    @Override
+    public void finCodeCadet(CodeCadet codeCadet) {
+
+    }
 }
