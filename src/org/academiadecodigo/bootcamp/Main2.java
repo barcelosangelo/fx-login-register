bb3815ff0b46c548d77da460fa58f47e3fbaeeef
Angelo Barcelos <barcelos.angelo@gmail.com>
2018-01-04 09:26:40 -0100

    old

--- src/org/academiadecodigo/bootcamp/Main2.java
+++ src/org/academiadecodigo/bootcamp/Main2.java
@@ -1,4 +1,10 @@
 package org.academiadecodigo.bootcamp;
 
-public class main2 {
+public class Main2 {
+
+    //public static void main(String[] args) {
+
+        Bootcamp bootcamp = new Bootcamp(1,"Praia da Vitória",12-07-2017,12-07-2017);
+
+    //}
 }
